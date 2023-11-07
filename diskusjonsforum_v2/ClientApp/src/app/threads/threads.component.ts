4bd1a8dbf439e16804c40e0a252dae6c243151ef
Saloni Arora Chitkara <s364538@oslomet.no>
2023-11-07 19:16:51 +0100

    Fulgte Bulding Component Demoen men må endres

--- diskusjonsforum_v2/ClientApp/src/app/threads/threads.component.ts
+++ diskusjonsforum_v2/ClientApp/src/app/threads/threads.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-threads-component',
-  templateUrl: './threads.component.html'
+  selector: 'app-thread',
+  templateUrl: './threads.component.html',
+  styleUrls: ['./threads.component.css']
 })
+export class ThreadComponent implements OnInit {
+  thread: any;
+  userId: string;
+  userIsAdmin: boolean;
+  viewTitle: string = 'Thread';
 
-export class ThreadsComponent {
+  constructor(private http: HttpClient) { }
 
+  ngOnInit(): void {
+    this.userId = ''; // Fetch the current user's ID
+    this.userIsAdmin = false; // Check if the user is an admin
+
+    // Fetch thread details
+    this.http.get<any>('api/threads/threadId').subscribe((thread) => {
+      this.thread = thread;
+    });
+  }
+
+  // Function to add a comment
+  addComment() {
+    // Logic to add a comment
+  }
+
+  // Function to edit a comment
+  editComment(commentId: number) {
+    // Logic to edit a comment
+  }
+
+  // Function to delete a comment
+  deleteComment(commentId: number) {
+    // Logic to delete a comment
+  }
 }
-
