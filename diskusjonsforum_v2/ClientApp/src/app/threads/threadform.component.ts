79a167c6d89a0b0437ff2f236ddaa69a45699c99
Jenny Hovland <jenny.hovland@hva.nl>
2023-11-25 17:31:06 +0100

    Added http post to threadform and new create in thread controller

--- diskusjonsforum_v2/ClientApp/src/app/threads/threadform.component.ts
+++ diskusjonsforum_v2/ClientApp/src/app/threads/threadform.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-threads-threadform',
@@ -10,11 +12,11 @@ import { FormControl, FormGroup, Validators, FormBuilder, ReactiveFormsModule } 
 export class ThreadformComponent {
   threadForm: FormGroup;
 
-  constructor(private _formBuilder: FormBuilder) {
+  constructor(private _formBuilder: FormBuilder, private _router: Router, private _http: HttpClient) {
     this.threadForm = _formBuilder.group({
       title: ['', Validators.required],
       body: ['', Validators.required]
-    })
+    });
   }
 
 
@@ -23,5 +25,20 @@ export class ThreadformComponent {
     console.log(this.threadForm);
     console.log('The item ' + this.threadForm.value.title + ' is created.');
     console.log(this.threadForm.touched);
+    const newThread = this.threadForm.value;
+    const createUrl = "api/thread/create";
+    this._http.post<any>(createUrl, newThread).subscribe(response => {
+      if (response.success) {
+        console.log(response.message);
+        this._router.navigate(['/threads']);
+      }
+      else {
+        console.log('Thread creation failed');
+      }
+    });
+  }
+
+  backToThreads() {
+    this._router.navigate(['/threads']);
   }
 }
