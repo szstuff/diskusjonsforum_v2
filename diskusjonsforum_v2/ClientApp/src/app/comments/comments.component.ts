39e36fa6eba6fb5910a311e0cd0c2b0ca388c3b2
linnvu <linn20011@gmail.com>
2023-11-25 20:51:40 +0100

    demo: http post request for comment, ikke hele

--- diskusjonsforum_v2/ClientApp/src/app/comments/comments.component.ts
+++ diskusjonsforum_v2/ClientApp/src/app/comments/comments.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Comment } from './comments';
+import {ThreadService} from "../threads/threads.service";
+import {Router} from "@angular/router";
+import {FormBuilder} from "@angular/forms";
+import {CommentsService} from "./comments.service";
+
+
 //import { User } from '../users/users';
 
 @Component({
@@ -10,10 +16,11 @@ import { Comment } from './comments';
 })
 
 export class CommentsComponent implements OnInit {
+  viewTitle: string = "Table";
   comments: Comment[] = [];
   //users: User[] = []; // Assuming you want to use the User interface here
 
-  constructor(private _http: HttpClient) { }
+
 
   ngOnInit(): void {
     console.log('CommentsComponent created');
@@ -21,13 +28,16 @@ export class CommentsComponent implements OnInit {
     // If you want to get users, you can call a method to fetch them here
     // this.getUsers();
   }
-
+  constructor(private commentsService : CommentsService, private _http: HttpClient , private _router: HttpClient) {
+  }
   getComments(): void{
     this._http.get<Comment[]>('api/comments').subscribe(data => {
       console.log('All', JSON.stringify(data));
       this.comments = data;
     });
   }
+
+
 
   /*
   getUsers(): void {
@@ -51,4 +61,5 @@ export class CommentsComponent implements OnInit {
   deleteComment(commentId: number) {
     // Logic to delete a comment
   }
+
 }
--- diskusjonsforum_v2/ClientApp/src/app/comments/comments.service.ts
+++ diskusjonsforum_v2/ClientApp/src/app/comments/comments.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Observable} from "rxjs";
+import {Comment} from "./comments";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
+  private apiUrl = 'api/comments';
+  constructor(private _http: HttpClient) { }
 
-  constructor() { }
+  getComments(): Observable<Comment[]> {
+    return this._http.get<Comment[]>(this.apiUrl);
+  }
+
+  createComment(newComment: Comment):Observable<any>{
+    const createUrl ="api/comment/create";
+    return this._http.post<any>(createUrl, newComment);
+  }
 }
+
+
