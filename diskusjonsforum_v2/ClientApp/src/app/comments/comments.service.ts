59f4f203cf2564559990cc3f4339944d1c92b8ac
Saloni Arora Chitkara <s364538@oslomet.no>
2023-11-26 16:24:13 +0100

    Fikset CommentController.cs comments.service.ts og repositories. Implementert bedre angular på ThreadController også

--- diskusjonsforum_v2/ClientApp/src/app/comments/comments.service.ts
+++ diskusjonsforum_v2/ClientApp/src/app/comments/comments.service.ts
@@ -15,9 +15,18 @@ export class CommentsService {
   }
 
   createComment(newComment: Comment):Observable<any>{
-    const createUrl ="api/comment/create";
-    return this._http.post<any>(createUrl, newComment);
+    const url = `${this.apiUrl}/create`;
+    return this._http.post<any>(url, newComment);
+  }
+
+  updateComment(comment: Comment): Observable<any> {
+    const url = `${this.apiUrl}/update/${comment.commentId}`;
+    return this._http.put<any>(url, comment);
+  }
+
+  deleteComment(commentId: number): Observable<any> {
+    const url = `${this.apiUrl}/deleteComment/${commentId}`;
+    return this._http.delete<any>(url);
   }
 }
-
 
