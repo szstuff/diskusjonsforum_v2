f76d9d5e46ff0c8cab3b7099d4acd62692a6d926
linnvu <linn20011@gmail.com>
2023-11-26 20:19:24 +0100

    comment

--- diskusjonsforum_v2/ClientApp/src/app/comments/comments.service.ts
+++ diskusjonsforum_v2/ClientApp/src/app/comments/comments.service.ts
@@ -7,26 +7,27 @@ import {HttpClient} from "@angular/common/http";
   providedIn: 'root'
 })
 export class CommentsService {
-  private apiUrl = 'api/comments';
+  private apiUrl = 'api/comments'; // navigates to the API endpoints for comments
   constructor(private _http: HttpClient) { }
 
   getCommentsByThreadId(parentThreadId: number): Observable<Comment[]> {
     return this._http.get<Comment[]>(`${this.apiUrl}/getByThread/${parentThreadId}`);
   }
 
+  //creates a new comment
   createComment(newComment: Comment):Observable<any>{
     const url = `${this.apiUrl}/create`;
-    return this._http.post<any>(url, newComment);
+    return this._http.post<any>(url, newComment); // makes a Post request with the data from the new comment
   }
 
-  updateComment(comment: Comment): Observable<any> {
+  updateComment(comment: Comment): Observable<any> { // updates the comment that already exists
     const url = `${this.apiUrl}/update/${comment.commentId}`;
-    return this._http.put<any>(url, comment);
+    return this._http.put<any>(url, comment); // makes a PUT request  with the updated comment
   }
 
-  deleteComment(commentId: number): Observable<any> {
+  deleteComment(commentId: number): Observable<any> { // deletes comment by specified commentId
     const url = `${this.apiUrl}/deleteComment/${commentId}`;
-    return this._http.delete<any>(url);
+    return this._http.delete<any>(url); // makes a DELETE request
   }
 }
 
