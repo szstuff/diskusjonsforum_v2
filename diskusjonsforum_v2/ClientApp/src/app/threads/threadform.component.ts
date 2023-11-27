f5b0f8e592180f8b337ff927ec6061644b161fd3
szstuff <stilianzagorov1@gmail.com>
2023-11-27 12:42:12 +0100

    Rettet feil på threadform komponentene. Man kan nå opprette tråder og input validation fungerer som forventet. Datoer blir satt riktig

--- diskusjonsforum_v2/ClientApp/src/app/threads/threadform.component.ts
+++ diskusjonsforum_v2/ClientApp/src/app/threads/threadform.component.ts
@@ -3,38 +3,41 @@ import { FormControl, FormGroup, Validators, FormBuilder, ReactiveFormsModule } 
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { ThreadService } from './threads.service';
+import {Comment} from "../comments/comments";
 
 @Component({
-  selector: 'app-threads-threadform', // custom HTML tag
-  templateUrl: './threadform.component.html', // path to the HTML file
+  selector: 'app-threads-threadform',
+  templateUrl: './threadform.component.html', // path to the HTML component
   //styleUrls: ['./thread-form.component.css'],
 })
 
 export class ThreadformComponent {
-  threadForm: FormGroup; // provides the object FormGRoup as threadForm
+  threadForm: FormGroup; // Initialise a FormGroup object
 
   constructor(
-    private _formBuilder: FormBuilder, // the form for creating threads
-    private _router: Router, //lets us nagivate to different routers within the angular application
-    private _threadService: ThreadService, //encapsulate functionality linked to ThreadService
-    private _http: HttpClient) // sends HTTP requests and receives HTTP responses
+    private _formBuilder: FormBuilder,
+    private _router: Router, // Initialise router object for navigation
+    private _threadService: ThreadService,
+    private _http: HttpClient)
   {
     this.threadForm = _formBuilder.group({
-      // uses _formBuilder to create threadForm a formgroup
-      title: ['', Validators.required], // Validators.required ensures the input title is not empty
-      body: ['', Validators.required] // ensures the input of the body is not empty
+      // Define FormBuilder input validation rules
+      createdBy: ['', Validators.required],
+      threadTitle: ['', Validators.required],
+      threadBody: ['', Validators.required],
+
     });
   }
 
 
   onSubmit() { // the method gets triggered when a thread is submitted
-    console.log("ThreadCreate from submitted:"); // Logs when the thread has been submitted
-    console.log(this.threadForm); // logs the object "threadForm"
-    console.log('The thread ' + this.threadForm.value.title + ' is created.'); // logs a message telling the thread has been created
-    console.log(this.threadForm.touched); //Logs if the threadForm control has been touched or not
-    const newThread = this.threadForm.value; //Gets the current values of the threadForm control
+    console.log("ThreadCreate from submitted:");
+    console.log(this.threadForm);
+    console.log('The thread ' + this.threadForm.value.title + ' is created.');
+    console.log(this.threadForm.touched);
+    const newThread = this.threadForm.value; // Creates a Thread object with values from the form
     const createUrl = "api/thread/create"; //navigates to the URL for creating new thread
-    this._threadService.createThread(newThread) //makes http post request with the data from newThread
+    this._threadService.createThread(newThread) //Send the new thread to ThreadService->ThreadController to save
       .subscribe(response => {
       if (response.success) {
         // if the response is a sucsess a message is logged from the server
