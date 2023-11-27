907d73174afc740dca9253db8fc2506667466247
Jenny Hovland <jenny.hovland@hva.nl>
2023-11-27 19:40:40 +0100

    endret search bar, design, fjerner skrift, og kan trykke på hele

--- diskusjonsforum_v2/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ diskusjonsforum_v2/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Thread } from '../threads/threads';
 import { NavMenuService } from './nav-menu.service';
-import { ThreadService } from "../threads/threads.service";
+import { ThreadService } from '../threads/threads.service';
 
 @Component({
   selector: 'app-nav-menu',
@@ -14,6 +14,24 @@ export class NavMenuComponent {
   searchResults: Thread[] = [];
   handlesearchinput: string = '';
 
+  // Add a reference to the search bar element
+  @ViewChild('searchBarElement') searchBarElement!: ElementRef;
+
+  // Event handler for document click
+  @HostListener('document:click', ['$event'])
+  handleDocumentClick(event: MouseEvent): void {
+    const clickedElement = event.target as HTMLElement;
+
+    // Check if the clicked element is outside the search bar
+    if (!this.searchBarElement.nativeElement.contains(clickedElement)) {
+      // Reset the search input when clicking outside the search bar
+      this.handlesearchinput = '';
+
+      // Close the search results dropdown
+      this.searchResults = [];
+    }
+  }
+
   toggle() {
     this.isExpanded = !this.isExpanded;
   }
@@ -22,7 +40,7 @@ export class NavMenuComponent {
     private navMenuService: NavMenuService,
     private threadService: ThreadService,
     private router: Router
-  ) {}
+  ) { }
 
   handleSearchInput() {
     const searchTerm = this.handlesearchinput.trim().toLowerCase();
@@ -40,6 +58,7 @@ export class NavMenuComponent {
       this.searchResults = [];
     }
   }
+
   navigateToThreadform(thread: Thread) {
     this.router.navigate(['/thread-view', thread.threadId]);
   }
