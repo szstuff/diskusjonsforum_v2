f8f0622669a1799763da52b9ecc31806ab28a4cd
linnvu <linn20011@gmail.com>
2023-11-28 23:13:08 +0100

    f

---
+++ xUnitTestDiskusjonsforum/Controllers/CommentControllerTests.cs
@@ -0,0 +1,14 @@
+using diskusjonsforum_v2.Controllers;
+using diskusjonsforum_v2.DAL;
+using Microsoft.AspNetCore.Mvc;
+using diskusjonsforum_v2.Models;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Thread = diskusjonsforum_v2.Models.Thread;
+
+namespace xUnitTestDiskusjonsforum.Controllers;
+
+public class CommentControllerTests
+{
+    
+}
